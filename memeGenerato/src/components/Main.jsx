7f8a1d426b63bc117b45acaef334f7bee5bf6b23
Abdulrahim Muhammad <abdelrheem2204@gmail.com>
2026-07-20 21:50:16 +0300

    using useEffect

--- memeGenerato/src/components/Main.jsx
+++ memeGenerato/src/components/Main.jsx
@@ -2,6 +2,14 @@ import React from "react"
 
 export default function Main() {
 
+    const [allMemes, setAllMemes] = React.useState([]);
+
+    React.useEffect(()=> {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes));
+    }, []);
+
     const [meme, setMeme] = React.useState({
         topText: "One does not simply",
         bottomText: "Walk into Mordor",
